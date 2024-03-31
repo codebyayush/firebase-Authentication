d7d079a3e405a847c2432fb1b2af882079a15a11
codebyayush <ayushpal6939@gmail.com>
2024-03-31 15:17:37 +0530

    auto-logout

--- src/Store/AuthCtx.js
+++ src/Store/AuthCtx.js
@@ -1,41 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const AuthCtx = React.createContext({
-    token: '',
-    isLoggedIn: false,
-    login: (token) => {},
-    logout: () => {}
-})
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
 
 export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem("TOKEN");
+  const [token, setToken] = useState(initialToken);
 
-    const initialToken = localStorage.getItem('TOKEN')
-    const [token, setToken] = useState(initialToken);
-    
-    const userIsLoggedIn = !!token;
+  const userIsLoggedIn = !!token;
 
-    console.log(userIsLoggedIn)
+  console.log(userIsLoggedIn);
 
-    const loginHandler = (token) => {
-        setToken(token);
-        localStorage.setItem("TOKEN", token);
-    }
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("TOKEN", token);
+    setTimeout(() => {
+      localStorage.removeItem("TOKEN");
+    }, 5 * 60 * 1000);
+  };
 
-    const logoutHandler = () => {
-        setToken(null)
-        localStorage.removeItem("TOKEN");
-    }
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("TOKEN");
+  };
 
-    const ctxValue = {
-        token: token,
-        isLoggedIn: userIsLoggedIn,
-        login: loginHandler,
-        logout: logoutHandler,
-    }
+  const ctxValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
 
-    return <AuthCtx.Provider value={ctxValue}>
-        {props.children}
-    </AuthCtx.Provider>
-}
+  return <AuthCtx.Provider value={ctxValue}>{props.children}</AuthCtx.Provider>;
+};
 
 export default AuthCtx;
